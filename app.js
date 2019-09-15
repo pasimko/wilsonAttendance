585259604d76f69e7e76e2a3624ff3eb0e22f39e
Paul <pasimko13@gmail.com>
2019-09-15 00:20:48 -0700

    Parse ip

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var http = require("http");
 var fs = require("fs");
 var express = require("express");
+var sys = require("sys");
+const { exec } = require("child_process");
+
+var usersJSON = fs.readFileSync("users.json"); 
 
 var app = express();
 
@@ -18,12 +22,20 @@ app.get('/public/thanks.html',function(req,res) {
   res.sendFile(__dirname+'/public/thanks.html');
 });
 
-
 // Access the parse results as request.body
 app.post('/', function(request, response){
     console.log(request.body.name);
     console.log(request.body.email);
-    console.log(request.connection.remoteAddress);
+    let ip = request.connection.remoteAddress;
+    console.log(ip);
+    
+    exec(`echo ${ip} | sed -r 's/[^0-9.]+//'`, (err, stdout, stderr) => {
+        if (err) {console.log("Error parsing IP");}
+        else {
+            console.log(`stdout: ${stdout}`);
+        }
+    });
+
     response.writeHead(302, {'Location': '/public/thanks.html'});
     response.end();
 });
